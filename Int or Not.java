4c98cab08f4e55aa2c26c3798e785440caefc2b6
JS <112610266+Js-s-c@users.noreply.github.com>
2022-09-11 23:39:55 +0530

    Create Int or Not.java

---
+++ Int or Not.java
@@ -0,0 +1,12 @@
+//cheking the input is intiger or not
+//code:-
+import java.util.Scanner;
+
+public class check {
+    public static void main(String[] args) {
+        System.out.println("Give input");
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.hasNextInt());
+
+    }
+}
